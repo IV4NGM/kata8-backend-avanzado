16bd4e068b57727bf464def0aadf3f92368e4006
IvanGM <ivangm_01@hotmail.com>
2024-01-25 21:16:13 -0600

    Archivos de clase 1 añadidos

---
+++ clase1/backend/routes/tareasRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const { getTareas, createTareas, updateTareas, deleteTareas } = require('../controllers/tareasController')
+
+router.get('/', getTareas)
+router.post('/', createTareas)
+
+// La siguiente línea equivale a las dos anteriores:
+// router.route('/').get(getTareas).post(createTareas)
+
+router.put('/:id', updateTareas)
+router.delete('/:id', deleteTareas)
+// router.route('/:id').put(updateTareas).delete(deleteTareas)
+
+module.exports = router
