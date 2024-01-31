faa12d097951d6659fe9d1923208e3cd1be718ca
IvanGM <ivangm_01@hotmail.com>
2024-01-30 20:38:50 -0600

    feat: Archivos de clase añadidos

--- clase1/backend/controllers/tareasController.js
+++ clase1/backend/controllers/tareasController.js
@@ -1,18 +1,25 @@
-const getTareas = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getTareas = asyncHandler(async (req, res) => {
   res.status(200).json({ mensaje: 'Get Tareas' })
-}
+})
 
-const createTareas = (req, res) => {
+const createTareas = asyncHandler(async (req, res) => {
+  if (!req.body.descripcion) {
+    res.status(400)
+    throw new Error('Por favor teclea una descripción')
+  }
+
   res.status(201).json({ mensaje: 'Create tareas' })
-}
+})
 
-const updateTareas = (req, res) => {
+const updateTareas = asyncHandler(async (req, res) => {
   res.status(200).json({ mensaje: `Modificar la tarea con id ${req.params.id}` })
-}
+})
 
-const deleteTareas = (req, res) => {
+const deleteTareas = asyncHandler(async (req, res) => {
   res.status(200).json({ id: req.params.id })
-}
+})
 
 module.exports = {
   getTareas,
